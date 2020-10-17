caf0a4be947923f68f4a0ef854f5dfa1644f2e88
athBlank <athibet@home.co.th>
2020-10-18 01:42:10 +0700

    - init bluetooth lib - add runtime permission android

--- bluetoothscan/App.js
+++ bluetoothscan/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React , {useEffect , useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,13 +15,48 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-
+import { PermissionsAndroid } from 'react-native';
+import { Platform } from 'react-native';
 
 
 const App = () => {
+  const [isPermissionAndroidOK, setisPermissionAndroidOK] = useState(false)
+
+  useEffect(() => {
+
+    if (Platform.Version >= 23) {
+      console.log('Running on Nougat!');
+      setisPermissionAndroidOK(true);
+      requestCameraPermission();
+    }
+    console.log("5555");
+    return () => {
+      
+    }
+  }, [])
+
+
+  const requestCameraPermission =  async() => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        {
+          'title': 'INBODY App Location Permission',
+          'message': 'INBODY App needs access to your location '
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the Location")
+      } else {
+        console.log("Location permission denied")
+      }
+    } catch (err) {
+      console.warn(err)
+    }
+  }
   return(
     <View style = {{flex : 1 ,justifyContent : 'center' ,alignItems : 'center'}}>
-      <Text>Hi</Text>
+      <Text>{isPermissionAndroidOK ? "ok" : "bad"}</Text>
       
     </View>
   )
