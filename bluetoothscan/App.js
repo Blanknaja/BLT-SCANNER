2f76244f899e935307c1ff7b0f8fa5d4aa603d62
athBlank <athibet@home.co.th>
2020-11-13 21:33:29 +0700

    no message

--- bluetoothscan/App.js
+++ bluetoothscan/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React , {useEffect , useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,52 +14,120 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
+  NativeModules,
+  NativeEventEmitter,
 } from 'react-native';
-import { PermissionsAndroid } from 'react-native';
-import { Platform } from 'react-native';
+import {PermissionsAndroid} from 'react-native';
+import {Platform} from 'react-native';
+import BleManager from 'react-native-ble-manager';
 
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 const App = () => {
-  const [isPermissionAndroidOK, setisPermissionAndroidOK] = useState(false)
+  const [isPermissionAndroidOK, setisPermissionAndroidOK] = useState(false);
 
   useEffect(() => {
-
     if (Platform.Version >= 23) {
-      console.log('Running on Nougat!');
       setisPermissionAndroidOK(true);
-      requestCameraPermission();
+      requestLocationPermission();
+      BleManager.start({showAlert: false}).then(() => {
+        // Success code
+        console.log('Module initialized');
+      });
+
+      bleManagerEmitter.addListener(
+        'BleManagerDiscoverPeripheral',
+        handleDiscoverPeripheral,
+      );
+
+      scanDevice();
     }
-    console.log("5555");
     return () => {
-      
+      bleManagerEmitter.removeListener('BleManagerDiscoverPeripheral');
+    };
+  }, []);
+
+  const handleDiscoverPeripheral = (peripheral) => {
+    console.log("peripheral " ,peripheral)
+    console.log('peripheral name ', peripheral.name);
+    console.log('peripheral id ', peripheral.id);
+
+    if (peripheral.id == '73:E5:12:5F:CB:E8') {
+      BleManager.stopScan();
+
+      BleManager.connect('73:E5:12:5F:CB:E8')
+        .then(() => {
+          // Success code
+          console.log('Connected');
+        })
+        .catch((error) => {
+          // Failure code
+          console.log(error);
+        });
+
+        BleManager.retrieveServices("73:E5:12:5F:CB:E8").then(
+          (peripheralInfo) => {
+            // Success code
+            console.log("Peripheral info:", peripheralInfo);
+          }
+        );
     }
-  }, [])
+  };
 
-
-  const requestCameraPermission =  async() => {
+  const requestLocationPermission = async () => {
     try {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
         {
-          'title': 'INBODY App Location Permission',
-          'message': 'INBODY App needs access to your location '
-        }
-      )
+          title: 'INBODY App Location Permission',
+          message: 'INBODY App needs access to your location ',
+        },
+      );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("You can use the Location")
+        console.log('You can use the Location');
       } else {
-        console.log("Location permission denied")
+        console.log('Location permission denied');
       }
     } catch (err) {
-      console.warn(err)
+      console.warn(err);
     }
-  }
-  return(
-    <View style = {{flex : 1 ,justifyContent : 'center' ,alignItems : 'center'}}>
-      <Text>{isPermissionAndroidOK ? "ok" : "bad"}</Text>
-      
+  };
+
+  const scanBle = () => {
+    BleManager.enableBluetooth()
+      .then(() => {
+        // Success code
+        console.log('The bluetooth is already enabled or the user confirm');
+      })
+      .catch((error) => {
+        // Failure code
+        console.log('The user refuse to enable bluetooth');
+      });
+  };
+
+  const scanDevice = () => {
+    BleManager.scan([], 30);
+
+    // DE:35:57:84:DF:AFz
+
+    //   BleManager.connect("40:5E:77:BE:D4:12")
+    // .then(() => {
+    //   // Success code
+    //   console.log("Connected");
+    // })
+    // .catch((error) => {
+    //   // Failure code
+    //   console.log(error);
+    // });
+  };
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>{isPermissionAndroidOK ? 'ok' : 'bad'}</Text>
+      <Button title="SCAN" onPress={scanBle} />
     </View>
-  )
-}
+  );
+};
 
 export default App;
